6fa2f928c212297ed5006eea83f52e9109d4b03c
Jeonghyeon <dalpaeng2@gmail.com>
2024-01-23 20:40:02 +0900

    - 타입 오류 수정

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,7 @@ import { IoIosLogOut } from 'react-icons/io';
 import { RxAvatar } from 'react-icons/rx';
 import { FaRegHeart } from 'react-icons/fa';
 import { FaRegComment } from 'react-icons/fa';
+import Image from 'next/image';
 
 export default async function Home() {
   const cookieStore = cookies();
@@ -111,7 +112,7 @@ type PostProps = {
     photo_url: string;
     profiles: {
       email: string;
-    };
+    }[];
   };
 };
 
@@ -121,19 +122,19 @@ function Post({ post }: PostProps) {
       <div className="flex justify-between items-center mb-2">
         <div className="flex items-center gap-2">
           <div className="bg-blue-200 w-6 h-6 rounded-full"></div>
-          <div className="text-sm">{post?.profiles.email}</div>
+          <div className="text-sm">{post?.profiles[0]?.email}</div>
           <div className="text-xs text-slate-600">1일</div>
         </div>
         <div>...</div>
       </div>
-      <img src={post?.photo_url} alt="" className="w-full" />
+      <Image src={post?.photo_url} alt="" className="w-full" />
       <div className="flex gap-3 text-lg">
         <FaRegHeart />
         <FaRegComment />
       </div>
       <div className="text-sm">좋아요 100개</div>
       <div>
-        {post?.profiles.email} - {post?.content}
+        {post?.profiles[0]?.email} - {post?.content}
       </div>
       <div className="text-sm text-slate-600">댓글 100개 모두 보기</div>
       <input
