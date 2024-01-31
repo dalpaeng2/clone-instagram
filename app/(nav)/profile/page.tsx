9b81b376d16e5c616adc50938dd7ccad629c775e
Jeonghyeon <dalpaeng2@gmail.com>
2024-01-31 16:51:06 +0900

    profile layout 작성

--- app/(nav)/profile/page.tsx
+++ app/(nav)/profile/page.tsx
@@ -1,7 +1,44 @@
+/* eslint-disable @next/next/no-img-element */
+import { supabaseServer } from '@/lib/supabase/server';
 import React from 'react';
 
-function Profile() {
-  return <div>Profile</div>;
+async function Profile() {
+  const supabase = supabaseServer();
+
+  const photos = [1, 2, 3, 4, 5, 6, 7];
+
+  return (
+    <>
+      <div className="flex flex-col gap-8 max-w-3xl">
+        <div className="flex gap-8">
+          <div className="bg-slate-400 rounded-full w-32 h-32"></div>
+          <div className="flex flex-col justify-between">
+            <div>
+              <div className="text-2xl">username</div>
+            </div>
+            <div className="flex gap-4">
+              <span>게시물 1</span>
+              <span>팔로워 1</span>
+              <span>팔로우 1</span>
+            </div>
+            <div>
+              <div className="text-sm">이름</div>
+            </div>
+          </div>
+        </div>
+        <div className="grid grid-cols-3 place-items-stretch gap-2 w-3xl">
+          {photos.map((photo) => (
+            <img
+              key={photo}
+              className="bg-slate-400 object-cover"
+              src="https://images.unsplash.com/photo-1554629947-334ff61d85dc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1024&h=1280&q=80"
+              alt=""
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Profile;
